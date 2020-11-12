4e5181a881980fc8fe17649623b52b400324842d
Jeffrey Westerkamp <jeffrey@pxlwidgets.com>
2020-11-12 22:55:52 +0100

    Added tag-name recognition to `queryAll`

--- src/queryAll.ts
+++ src/queryAll.ts
@@ -1,3 +1,23 @@
-export const queryAll = <T extends HTMLElement = HTMLElement>(selector: string, scope: ParentNode = document): T[] => (
-    Array.prototype.slice.call(scope.querySelectorAll(selector))
-)
+/**
+ * Calls `querySelectorAll` with given `selector` on given `scope`, or on `document` by default when the
+ * scope is omitted. Returns an array containing the found elements.
+ *
+ * @example
+ * ```typescript
+ * // Recognizes keys of HTMLElementTagNameMap:
+ * const anchors = queryAll('a') // HTMLAnchorElement[]
+ *
+ * // Recognizes keys of SVGElementTagNameMap:
+ * const paths = queryAll('path') // SVGPathElement[]
+ *
+ * // Defaults to Element, or accepts an explicit type argument for the searched elements:
+ * const elements = queryAll('.some-element') // Element[]
+ * const buttons = queryAll<HTMLButtonElement>('.my-button') // HTMLButtonElement[]
+ * ```
+ */
+export function queryAll<K extends keyof HTMLElementTagNameMap>(selector: K, scope?: ParentNode): HTMLElementTagNameMap[K][]
+export function queryAll<K extends keyof SVGElementTagNameMap>(selector: K, scope?: ParentNode): SVGElementTagNameMap[K][]
+export function queryAll<T extends Element>(selector: string, scope?: ParentNode): T[]
+export function queryAll(selector: string, scope: ParentNode = document) {
+    return Array.prototype.slice.call(scope.querySelectorAll(selector))
+}
