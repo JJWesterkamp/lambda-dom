1a5a73e24d108a6411fbc132a625f7d0fa73c673
Jeffrey Westerkamp <jeffrey@pxlwidgets.com>
2020-11-13 20:01:14 +0100

    Added examples to `deferFrames`

--- src/deferFrames.ts
+++ src/deferFrames.ts
@@ -1,7 +1,25 @@
 /**
- * Takes a number `n` and a callback function, and executes the callback function after `n`
+ * Takes a positive (>= 0) integer `n` and a callback function, and executes the callback function after `n`
  * animation frames have passed.
+ *
  * @Todo Add the possibility to cancel. The requires the request ID of the latest request.
+ *
+ * @example
+ * ```typescript
+ * declare const f: () => void
+ * // Run `f` after ten animation frames
+ * deferFrames(10, f)
+ *
+ * // The following 2 statements are equivalent:
+ * deferFrames(0, f)
+ * requestAnimationFrame(f)
+ *
+ * // And the following 2 statements are equivalent:
+ * deferFrames(1, f)
+ * requestAnimationFrame(() => requestAnimationFrame(f))
+ *
+ * // Etc..
+ * ```
  */
 export const deferFrames = (n: number, handler: () => any): void => {
     n === 0 ? requestAnimationFrame(handler) : deferFrames(n - 1, () => requestAnimationFrame(handler))
