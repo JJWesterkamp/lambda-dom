7b390f5559b9b28cc5fe913c994f3cfa774b7d00
Jeffrey Westerkamp <jeffrey@pxlwidgets.com>
2020-11-13 20:17:41 +0100

    cleanup / docs

--- src/removeClasses.ts
+++ src/removeClasses.ts
@@ -7,13 +7,14 @@ import { toggleClasses } from './toggleClasses'
  * @example
  *
  * ```javascript
+ * declare const someElement: Element
+ * declare const elements: Element[]
+ *
  * // You can either partially apply removeClasses:
  * const removeTwoClasses = removeClasses('class-one', 'class-two')
- * const elements = document.querySelectorAll('.some-element')
  * elements.forEach(removeTwoClasses)
  *
  * // Or execute removeClasses in one go:
- * const element = document.getElementById('some-element')
  * removeClasses('class-one', 'class-two', 'even-more-classes')(element)
  * ```
  */
--- src/setMeta.ts
+++ src/setMeta.ts
@@ -1,25 +1,31 @@
 /**
+ * Takes a string name and returns a new function that takes a string content, and
+ * then updates the `<meta>` tag with the given name if it exists, or otherwise
+ * creates a new one. The meta element to which the content value was set is returned
+ * for reference.
  *
+ * When a new element is created it will be appended to the end of `<head>`.
+ *
+ * @example
+ * ```typescript
+ * const element = setMeta('foo')('bar')
+ * ```
+ * ```html
+ * <!-- This updates or creates the following element -->
+ * <meta name="foo" content="bar">
+ * ```
  */
-export function setMeta(name: string): (value: string) => void
-export function setMeta(name: string, value: string): void
-export function setMeta(name: string, value?: string) {
-
-    if (value === undefined) {
-        return (value: string) => setMeta(name, value)
-    }
+export const setMeta = (name: string) => (content: string): HTMLMetaElement => {
 
     let element: HTMLMetaElement | null = document.querySelector(`meta[name="${name}"]`)
 
     if ( ! element) {
-        element = document.createElement('meta')
+        element      = document.createElement('meta')
         element.name = name
-
-        const head = document.head || document.getElementsByTagName("head")[0]
-
+        const head   = document.head || document.getElementsByTagName("head")[0]
         head.appendChild(element)
     }
 
-    element.content = value
-    return
+    element.content = content
+    return element
 }
