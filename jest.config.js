75f2bb6161b4db6443f4aff11f2cd9fa5dfba310
Jeffrey Westerkamp <jeffreywesterkamp@hotmail.com>
2021-04-02 16:23:09 +0200

    cleanup - removed obsolete test config

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   verbose: true,
-  setupFiles: [
-    './test/mocks/dom.mock.ts',
-  ],
   collectCoverage: true,
   collectCoverageFrom: [
     './src/**/*.ts'
