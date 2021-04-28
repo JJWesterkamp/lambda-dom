f414c01b245c0ccdce2246ca2638c35d33c6e9ab
Jeffrey Westerkamp <jeffreywesterkamp@hotmail.com>
2021-04-28 18:19:49 +0200

    Provide option for non-minified UMD bundles

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Minified and non-minified UMD bundles
+
 ## [2.0.1] (2021-04-28)
 
 ### Fixed
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const { resolve } = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     mode: "production",
     target: 'es5',
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+          include: /\.min\.js$/,
+          extractComments: false,
+        })]
+    },
     entry: {
+        "lambda-dom.min": "./src/index.ts",
         "lambda-dom": "./src/index.ts",
     },
     output: {
@@ -25,5 +35,13 @@ module.exports = {
                 exclude: /node_modules/,
             }
         ],
-    }
+    },
+    plugins: [
+        new webpack.BannerPlugin([
+            `lambda-dom`,
+            `https://jjwesterkamp.github.io/lambda-dom`,
+            `(c) 2021 Jeffrey Westerkamp`,
+            `This software may be freely distributed under the MIT license.`,
+        ].join('\n')),
+    ]
 };
